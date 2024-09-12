f94194a0128a5d7d4d0f155c0a4b8f71c115b2b6
tejasg263 <tejasg263@gmail.com>
2024-09-12 15:19:05 +0000

    HomeSection changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import CursorFollower from "@/components/CursorFollower";
 import Navbar from "@/components/Navbar";
 import ContactSection from "@/components/sections/ContactSection";
-import HeroSection from "@/components/sections/HomeSection";
+import HomeSection from "@/components/sections/HomeSection";
 import ProjectSection from "@/components/sections/ProjectSection";
 import SkillSection from "@/components/sections/SkillSection";
 
 export default function Home() {
   return (
-    <main className="snap-y snap-mandatory h-screen overflow-x-hidden overflow-scroll scroll-smooth">
-      <Navbar />
-      <CursorFollower />
-      <HeroSection />
-      <SkillSection />
-      <ProjectSection />
-      <ContactSection />
-    </main>
-  );
+  <div className="w-screen">
+      <Navbar/>
+      <CursorFollower/>
+      <HomeSection />
+      <SkillSection/>
+      <ProjectSection/>
+      <ContactSection/>
+  </div>
+)
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -13,7 +13,7 @@ export default function Button({
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
       transition={{ type: "spring", stiffness: 400, damping: 10 }}
-      className="w-fit h-fit border px-16 py-7 rounded-full text-2xl font-montserrat flex gap-5"
+      className="w-fit h-fit border mi TV px-16 py-7 rounded-full text-2xl font-montserrat flex gap-5"
     >
       {children}
     </motion.button>
